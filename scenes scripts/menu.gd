3a6e0ab602e6972c3705f0499e3b294e4ec36049
Dranik544 <matsvej2011@gmail.com>
2025-11-01 17:56:20 +0000

    menu update 1 (buttons, transparent effect, new objects)

--- scenes scripts/menu.gd
+++ scenes scripts/menu.gd
@@ -4,7 +4,7 @@ var centercam
 var cam
 var freecam: bool = false
 var sens: float = 0.01
-var driftcam: float = 0.0
+var driftcam: float = 0
 var camscalewheel: float = 10.0
 
 
@@ -14,9 +14,10 @@ func _ready():
 
 func _input(event):
 	if event is InputEventMouseMotion and !freecam:
-		rotate_y(-event.relative.x * sens)
+		$"room1 1".rotate_y(event.relative.x * sens)
+		#centercam.rotate_y(-event.relative.x * sens)
 		centercam.rotate_x(-event.relative.y * sens)
-		centercam.rotation.x = clamp(centercam.rotation.x, -deg_to_rad(45), deg_to_rad(45))
+		#centercam.rotation.x = clamp(centercam.rotation.x, -deg_to_rad(45), deg_to_rad(45))
 		driftcam += event.relative.x / 1000
 		driftcam = clamp(driftcam, -0.05, 0.05)
 	if Input.is_action_just_pressed("F1"):
@@ -32,6 +33,18 @@ func _unhandled_input(event):
 	camscalewheel = clampf(camscalewheel, -2.5, 7.5)
 
 func _physics_process(delta: float) -> void:
+	$CanvasLayer/start.global_position = lerp($CanvasLayer/start.global_position, cam.unproject_position($"room1 1/room1/door".global_position), 25 * delta)
+	$CanvasLayer/exit.global_position = lerp($CanvasLayer/exit.global_position, cam.unproject_position($"room1 1/room1/MeshInstance3D".global_position), 25 * delta)
+	$CanvasLayer/settings.global_position = lerp($CanvasLayer/settings.global_position, cam.unproject_position($"room1 1/room1/MeshInstance3D2".global_position), 25 * delta)
+	$CanvasLayer/tutorial.global_position = lerp($CanvasLayer/tutorial.global_position, cam.unproject_position($"room1 1/room1/mesh1".global_position), 25 * delta)
+	
+	var mousepos = get_viewport().get_mouse_position()
+	var tr = 400
+	$CanvasLayer/start.modulate = Color(1, 1, 1, 1.0 - mousepos.distance_to($CanvasLayer/start.global_position) / tr)
+	$CanvasLayer/exit.modulate = Color(1, 1, 1, 1.0 - mousepos.distance_to($CanvasLayer/exit.global_position) / tr)
+	$CanvasLayer/settings.modulate = Color(1, 1, 1, 1.0 - mousepos.distance_to($CanvasLayer/settings.global_position) / tr)
+	$CanvasLayer/tutorial.modulate = Color(1, 1, 1, 1.0 - mousepos.distance_to($CanvasLayer/tutorial.global_position) / tr)
+	
 	if Input.is_action_pressed("RCM") or Input.is_action_pressed("CCM"):
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 		freecam = false
