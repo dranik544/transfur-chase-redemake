72ea382e7b17623015af22b8cf71a5d877e03867
Dranik544 <matsvej2011@gmail.com>
2025-12-18 15:28:52 +0300

    black slime + new sroom 2 loc

--- scenes scripts/slime_1.gd
+++ scenes scripts/slime_1.gd
@@ -3,8 +3,14 @@ extends StaticBody3D
 var time: float = 0.0
 var n: float = 0.5
 @export var speed: float = 2.0
+
+enum TYPE {whiteenemy, blackenemy}
+@export var curtype: TYPE = TYPE.whiteenemy
+
 @export var canBeEnemy: bool = true
-@export var enemyScene: PackedScene = preload("res://scenes scripts/enemy_1.tscn")
+@export var WhiteEnemyScene: PackedScene = preload("res://scenes scripts/enemy_1.tscn")
+@export var blackEnemyScene: PackedScene = preload("res://scenes scripts/enemy_3.tscn")
+var enemyScene
 @export var slimesprites = {
 	1: preload("res://sprites/slime5.png"),
 	2: preload("res://sprites/slime6.png"),
@@ -16,6 +22,14 @@ func _ready() -> void:
 	$Sprite3D.texture = slimesprites[randi_range(1, 3)]
 	speed = randf_range(1.75, 3.5)
 	add_to_group("slime")
+	
+	match curtype:
+		0:
+			enemyScene = WhiteEnemyScene
+			$Sprite3D.modulate = Color(1.0, 1.0, 1.0)
+		1:
+			enemyScene = blackEnemyScene
+			$Sprite3D.modulate = Color(0.100, 0.100, 0.100)
 
 func _process(delta):
 	time += delta
